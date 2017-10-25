1a6ac81137e12c52dbb1952dc4566e91c35318f5
kroll.anuar92@gmail.com <kroll.anuar92@gmail.com>
2017-10-25 22:27:52 +0900

    FILTERED LS

--- program5.js
+++ program5.js
@@ -5,8 +5,14 @@ var path = require('path')
 function read(callback){
 fs.readdir(process.argv[2], function pwd(err,data){
 	if(err)  return console.log(err)
-	console.log(data)
+//	console.log(data)
+	var check = "." + process.argv[3]
+//	console.log(check)
+	for(var i = 0;i<data.length;i++){
+		if(path.extname(data[i]) == check)
+			 console.log(data[i])
 
+	}
 });
 };
 function fileExtension(){
